4c821d6af8d87ed051aecbe5bf585ee8898c627c
John <128350391+alpaOo@users.noreply.github.com>
2024-08-20 00:01:40 +0800

    Implement structs for managing cache
    -- The 'Cache' struct contains a 'cacheMap', where each key is a URL
       string and the corresponding value is a 'cacheEntry' holding the data

--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -1,1 +1,16 @@
 package pokecache
+
+import (
+	"sync"
+	"time"
+)
+
+type Cache struct {
+	cacheMap map[string]cacheEntry
+	mu       sync.Mutex
+}
+
+type cacheEntry struct {
+	createdAt time.Time
+	val       []byte
+}
