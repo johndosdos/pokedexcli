7d51aec98b3ba631445b25e86e43624db8034a50
John <128350391+alpaOo@users.noreply.github.com>
2024-09-05 10:04:13 +0800

    Add structs for location exploration data
    The package contain structs for unmarshaling JSON data related to
    exploration location. The structs is used parse the API response when
    fetching location information.

---
+++ internal/explore/explore.go
@@ -0,0 +1,13 @@
+package explore
+
+type Response struct {
+	PokemonEncounters []PokemonEncounters `json:"pokemon_encounters"`
+}
+
+type PokemonEncounters struct {
+	Pokemon Pokemon `json:"pokemon"`
+}
+
+type Pokemon struct {
+	Name string `json:"name"`
+}
