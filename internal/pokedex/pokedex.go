1494db3793c8410ee747229b6a7578a94154fb63
John <128350391+alpaOo@users.noreply.github.com>
2024-09-17 03:05:05 +0800

    Create 'Read' function for pokemon data retrieval

--- internal/pokedex/pokedex.go
+++ internal/pokedex/pokedex.go
@@ -1,6 +1,8 @@
 package pokedex
 
 import (
+	"fmt"
+
 	"github.com/johndosdos/pokedexcli/internal/catch"
 )
 
@@ -10,4 +12,13 @@ func Add(pokemonData catch.PokemonData) {
 	pokemon := pokemonData.Name
 
 	dex[pokemon] = pokemonData
+}
+
+func Read(pokemon string) (catch.PokemonData, error) {
+	data, found := dex[pokemon]
+	if !found {
+		return catch.PokemonData{}, fmt.Errorf("Pokemon not found.")
+	}
+
+	return data, nil
 }
