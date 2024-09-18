bfb13606a76c26be6b7b834d014f85d31eefb679
John <128350391+alpaOo@users.noreply.github.com>
2024-09-18 23:03:46 +0800

    Update variables and dependencies

--- internal/actions/catch.go
+++ internal/actions/catch.go
@@ -7,40 +7,10 @@ import (
 	"time"
 
 	"github.com/johndosdos/pokedexcli/internal/pokedex"
+	"github.com/johndosdos/pokedexcli/internal/pokemon"
 )
 
-type PokemonData struct {
-	BaseExperience         int     `json:"base_experience"`
-	Height                 int     `json:"height"`
-	LocationAreaEncounters string  `json:"location_area_encounters"`
-	Name                   string  `json:"name"`
-	Stats                  []Stats `json:"stats"`
-	Types                  []Types `json:"types"`
-	Weight                 int     `json:"weight"`
-}
-type Stat struct {
-	Name string `json:"name"`
-	URL  string `json:"url"`
-}
-type Stats struct {
-	BaseStat int  `json:"base_stat"`
-	Effort   int  `json:"effort"`
-	Stat     Stat `json:"stat"`
-}
-type Type struct {
-	Name string `json:"name"`
-	URL  string `json:"url"`
-}
-type Types struct {
-	Slot int  `json:"slot"`
-	Type Type `json:"type"`
-}
-
-type PokemonSpecies struct {
-	CaptureRate int `json:"capture_rate"`
-}
-
-func Catch(pokemonData PokemonData, catchRateData PokemonSpecies) error {
+func Catch(pokemonData pokemon.PokemonData, catchRateData pokemon.PokemonSpecies) error {
 	if pokemonData.Name == "" {
 		return fmt.Errorf("No pokemon data found")
 	}
