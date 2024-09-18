8757793cc981eb067a96cd1cbdb0f4354c4d0efb
John <128350391+alpaOo@users.noreply.github.com>
2024-09-18 23:04:28 +0800

    Update variables and dependencies

--- internal/pokedex/pokedex.go
+++ internal/pokedex/pokedex.go
@@ -3,28 +3,28 @@ package pokedex
 import (
 	"fmt"
 
-	"github.com/johndosdos/pokedexcli/internal/catch"
+	"github.com/johndosdos/pokedexcli/internal/pokemon"
 )
 
 type Pokedex struct {
-	dex map[string]catch.PokemonData
+	dex map[string]pokemon.PokemonData
 }
 
 func NewPokedex() *Pokedex {
 	return &Pokedex{
-		dex: make(map[string]catch.PokemonData),
+		dex: make(map[string]pokemon.PokemonData),
 	}
 }
 
-func (p *Pokedex) Add(pokemonData catch.PokemonData) {
-	pokemon := pokemonData.Name
-	p.dex[pokemon] = pokemonData
+func (p *Pokedex) Add(pokemonData pokemon.PokemonData) {
+	pokemonName := pokemonData.Name
+	p.dex[pokemonName] = pokemonData
 }
 
-func (p *Pokedex) Read(pokemon string) (catch.PokemonData, error) {
-	data, found := p.dex[pokemon]
+func (p *Pokedex) Read(pokemonName string) (pokemon.PokemonData, error) {
+	data, found := p.dex[pokemonName]
 	if !found {
-		return catch.PokemonData{}, fmt.Errorf("Pokemon not found.")
+		return pokemon.PokemonData{}, fmt.Errorf("Pokemon not found.")
 	}
 
 	return data, nil
