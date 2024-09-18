55a27392c3fc224f0dd465d96d3bdb1a7ffbea81
John <128350391+alpaOo@users.noreply.github.com>
2024-09-18 22:56:41 +0800

    Create 'pokemon' package to handle related functions

---
+++ internal/pokemon/pokemon.go
@@ -0,0 +1,32 @@
+package pokemon
+
+type PokemonData struct {
+	BaseExperience         int     `json:"base_experience"`
+	Height                 int     `json:"height"`
+	LocationAreaEncounters string  `json:"location_area_encounters"`
+	Name                   string  `json:"name"`
+	Stats                  []Stats `json:"stats"`
+	Types                  []Types `json:"types"`
+	Weight                 int     `json:"weight"`
+}
+type Stat struct {
+	Name string `json:"name"`
+	URL  string `json:"url"`
+}
+type Stats struct {
+	BaseStat int  `json:"base_stat"`
+	Effort   int  `json:"effort"`
+	Stat     Stat `json:"stat"`
+}
+type Type struct {
+	Name string `json:"name"`
+	URL  string `json:"url"`
+}
+type Types struct {
+	Slot int  `json:"slot"`
+	Type Type `json:"type"`
+}
+
+type PokemonSpecies struct {
+	CaptureRate int `json:"capture_rate"`
+}
